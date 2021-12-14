caa50693802b274aa0ce612d235aef6fbd0bdcc0
FX Jollois <fxjolloisupd@gmail.com>
2021-12-14 10:57:38 +0100

    Update test-main-concepts.py

--- test-main-concepts.py
+++ test-main-concepts.py
@@ -10,8 +10,7 @@ import numpy as np
 st.title('Uber pickups in NYC')
 
 DATE_COLUMN = 'date/time'
-DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
-         'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
+DATA_URL = ('https://s3-us-west-2.amazonaws.com/streamlit-demo-data/uber-raw-data-sep14.csv.gz')
 
 @st.cache
 def load_data(nrows):
@@ -21,28 +20,24 @@ def load_data(nrows):
     data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
     return data
 
-# Create a text element and let the reader know the data is loading.
-data_load_state = st.text('Loading data...')
-# Load 10,000 rows of data into the dataframe.
+data_load_state = st.sidebar.text('Chargement des données...')
 data = load_data(10000)
-# Notify the reader that the data was successfully loaded.
-data_load_state.text("Done! (using st.cache)")
+data_load_state.text("Chargement effectué")
 
-if st.checkbox('Show raw data'):
-    st.subheader('Raw data')
+if st.sidebar.checkbox('Afficher données brutes'):
+    st.subheader('Données brutes')
     st.write(data)
 
-if st.checkbox('Show histogram'):
-    st.subheader('Number of pickups by hour')
+if st.sidebar.checkbox('Afficher la répartition horaire'):
+    st.subheader('Répartition horaire des arrêts')
     hist_values = np.histogram(
         data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
+    st.bar_chart(hist_values)
 
-st.bar_chart(hist_values)
+st.subheader('Cartes des prises en charges')
 
-st.subheader('Map of all pickups')
-
-hour_to_filter = st.slider('hour', 0, 23, 17)  # min: 0h, max: 23h, default: 17h
+hour_to_filter = st.sidebar.slider('Heure', 0, 23, 17)  # min: 0h, max: 23h, default: 17h
 filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
-st.subheader(f'Map of all pickups at {hour_to_filter}:00')
+st.caption(f'Carte à {hour_to_filter}:00')
 st.map(filtered_data)
 
